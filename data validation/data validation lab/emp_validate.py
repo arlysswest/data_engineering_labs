3e94c6115ee704b3d66a32a0959c60c30be3e5d3
Arlyss West <arlyss@pdx.edu>
2025-04-21 12:08:18 -0700

    initialize data validation lab

---
+++ data validation/data validation lab/emp_validate.py
@@ -0,0 +1,32 @@
+#Arlyss West CS410 data engineering 
+#data validation Lab "emp_validate.py"
+
+#2.Existance Assertion
+#Assertion: every record has a non-null name field
+#Write a python program called emp_validate.py (or something similar) that validates this assertion. The program should just count rows that do not satisfy this assertion. No need to modify or delete the invalid records. 
+#How many records violate this assertion?
+#Define a new, different Existence Assertion
+
+#3. Limit Assertion
+#assertion: every employee was hired no earlier than 2015
+#Add code to your program to validate this assertion 
+#How many records violate this assertion?
+#Define a new, different Limit Assertion
+
+#4. Intra-record Assertion
+#Assertion: each employee was born before they were hired
+#Add code to your program to validate this assertion
+#How many records violate this assertion?
+#Define a new, different Intra-record Assertion
+
+#6. Summary Assertion
+#Assertion: each city has more than one employee
+#Add code to your program to validate this assertion
+#Is the data set valid with respect to this assertion? 
+#Define a new, different Summary Assertion
+
+#7. Statistical Assertion
+#Assertion: the salaries are normally distributed
+#Add code to your program to validate this assertion
+#Is the data set valid with respect to this assertion? For this show a screenshot of a histogram of salaries and state whether the histogram appears to resemble a normal distribution.
+#Define a new, different Statistical Assertion
